3dd2b0d8f86e3f9187324ca16b2b9da71f255e64
KirukaFukamo <ivaylo.kardashev@gmail.com>
2023-10-13 12:24:11 +0300

    New function added.
    Function gross_profit_margin() is added.

---
+++ company_analyzer.py
@@ -0,0 +1,10 @@
+def gross_profit_margin(balance_sheet):
+    return balance_sheet.get("gross_profit") / balance_sheet.get("total_revenue") * 100
+
+
+'''Example'''
+balance_sheet = {"gross_profit": 104956000, "total_revenue": 274515000, "net_income": 57411000,
+                 "stock_equity": 201442000, "total_liabillities": 74467231, "total_assets": 96334, "inventar": 20497,
+                 "current_liabilities": 87812}
+
+
