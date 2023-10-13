046a28fd358305c432e9412359021c721e619d82
KirukaFukamo <ivaylo.kardashev@gmail.com>
2023-10-13 12:28:22 +0300

    New function is added.
    Function sign_for_companies_finance_health() was added.

--- company_analyzer.py
+++ company_analyzer.py
@@ -14,6 +14,14 @@ def quick_ratio(balance_sheet):
     return (balance_sheet.get("total_assets") - balance_sheet.get("inventar")) / balance_sheet.get("current_liabilities")
 
 
+def sign_for_companies_finance_health(balance_sheet):
+    if 0.5 < debt_to_equity_ratio(balance_sheet) < 1 or \
+            debt_to_equity_ratio(balance_sheet) < 0.5:
+        return "The company is in good finance state."
+    else:
+        return "The company isn't in good finance state."
+
+    
 '''Example'''
 balance_sheet = {"gross_profit": 104956000, "total_revenue": 274515000, "net_income": 57411000,
                  "stock_equity": 201442000, "total_liabillities": 74467231, "total_assets": 96334, "inventar": 20497,
