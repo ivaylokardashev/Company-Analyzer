c678c174afaf6185c0cb8e77501b2f266a32e166
KirukaFukamo <ivaylo.kardashev@gmail.com>
2023-10-13 12:25:15 +0300

    New function added.
    New function debt_to_equity_ratio() was added.

--- company_analyzer.py
+++ company_analyzer.py
@@ -2,6 +2,10 @@ def gross_profit_margin(balance_sheet):
     return balance_sheet.get("gross_profit") / balance_sheet.get("total_revenue") * 100
 
 
+def debt_to_equity_ratio(balance_sheet):
+    return balance_sheet.get("total_liabillities") / balance_sheet.get("stock_equity")
+
+
 '''Example'''
 balance_sheet = {"gross_profit": 104956000, "total_revenue": 274515000, "net_income": 57411000,
                  "stock_equity": 201442000, "total_liabillities": 74467231, "total_assets": 96334, "inventar": 20497,
