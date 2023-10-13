683149e4011a7a67e4cce7c24cdf1e2fbcf30bd3
KirukaFukamo <ivaylo.kardashev@gmail.com>
2023-10-13 12:26:20 +0300

    New function is added.
    Function current_ratio() was added.

--- company_analyzer.py
+++ company_analyzer.py
@@ -6,6 +6,10 @@ def debt_to_equity_ratio(balance_sheet):
     return balance_sheet.get("total_liabillities") / balance_sheet.get("stock_equity")
 
 
+def current_ratio(balance_sheet):
+    return balance_sheet.get("total_assets") / balance_sheet.get("current_liabilities")
+
+
 '''Example'''
 balance_sheet = {"gross_profit": 104956000, "total_revenue": 274515000, "net_income": 57411000,
                  "stock_equity": 201442000, "total_liabillities": 74467231, "total_assets": 96334, "inventar": 20497,
